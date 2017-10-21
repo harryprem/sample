0f36c7c8ea8e3eb8c86ac759a6650fe4cde6c8cf
harry.prem <harry.prem.harry@gmail.com>
2017-10-21 13:21:28 +0530

    Both files committed

---
+++ sample/samprog.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+
+
+int main()
+{
+	cout<<endl<<"This is a sample program"<<endl;
+
+	for( int i=0;i<10;i++)
+		cout<<endl<<"i"<<endl;
+	cout<<" Hi Harry "<<endl;
+
+	cout<<endl<<" Hi Harry ";
+
+return 0;
+}
+
+
